df7aeeb3c1c601a30307fa59f3602e253643fdc6
ckangwen <2437037469@qq.com>
2023-12-14 15:40:55 +0800

    fix: 解决Draggable无法拖动的问题

--- src/components/Draggable.tsx
+++ src/components/Draggable.tsx
@@ -1,15 +1,17 @@
 import { useRef } from "react";
 import useDraggable from "@/hooks/useDraggable";
 import { Clickable } from "~electron-utils/window/renderer";
+import { cn } from "@/libs/utils";
 
 interface DraggableProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-export default function Draggable(props: DraggableProps) {
+export default function Draggable({ className, ...props}: DraggableProps) {
   const draggableRef = useRef<HTMLDivElement>(null);
   const [x, y] = useDraggable(draggableRef);
   return (
     <Clickable
       ref={draggableRef}
+      className={cn("fixed", className)}
       style={{
         left: x + "px",
         top: y + "px",
